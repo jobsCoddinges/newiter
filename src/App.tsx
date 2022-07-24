8651e0c88f38097471f9248ade048561dfe4b704
jobsCoddinges <tlsrbcjf79@gmail.com>
2022-07-24 23:29:28 +0900

    two

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
+import fireBase from "./firebase";
 
+console.log(fireBase.options);
 function App() {
   return <div className="App"></div>;
 }
---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDynuf1ezZKJSzb2BQ75-x1ZHql30U3nVw",
+  authDomain: "newiter-60ede.firebaseapp.com",
+  projectId: "newiter-60ede",
+  storageBucket: "newiter-60ede.appspot.com",
+  messagingSenderId: "33396284574",
+  appId: "1:33396284574:web:ae9e5002193a571b53af8f",
+};
+
+// Initialize Firebase
+const fireBase = initializeApp(firebaseConfig);
+
+export default fireBase;
